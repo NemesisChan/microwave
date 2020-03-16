6eece9f0b493e1a06dd87ed2a1f4a06ce928c483
NemesisChan <maidendelta@gmail.com>
2020-03-16 16:27:38 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>電子レンジ加熱時間おしえて君</p>
+    <div>
+      <select id="watt">
+        <option value="100">100</option>
+        <option value="200">200</option>
+        <option value="500">500</option>
+        <option value="600">600</option>
+        <option value="700">700</option>
+        <option value="800">800</option>
+        <option value="900">900</option>
+        <option value="1000">1000</option>
+        <option value="1200">1200</option>
+        <option value="1500">1500</option>
+      </select>Wで
+      <input type="text" id="minute" v-model="defaultMinute" />分
+      <input type="text" id="second" v-model="defaultSecond" />秒
+    </div>
+    <br />
+    <div>
+      800Wで
+      分
+      秒 加熱
+      1200Wで
+      分
+      秒
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      defaultWatt: "500",
+      defaultMinute: "3",
+      defaultSecond: "00",
+      msg: "Hello World!"
+    };
+  },
+  methods: {
+    calculate: function() {}
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
